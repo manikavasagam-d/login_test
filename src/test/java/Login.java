853620732004c9faa4b066bf53e4398317788866
Anandveera <seamanand@gmail.com>
2021-05-25 21:25:59 +0530

    Initial test

---
+++ src/test/java/Login.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+    public String baseUrl = "https://staging.joinswoop.com/login";
+    public WebDriver driver ;
+    String driverPath = "G:\\chromedriver.exe";
+    @BeforeTest
+    public void launchBrowser() {
+        System.out.println("launching headless browser");
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+    }
+    @Test
+    public void verifyHomepageTitle() {
+        System.out.println("Verify home page title");
+        String expectedTitle = "Swoop";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @AfterTest
+    public void terminateBrowser(){
+        driver.close();
+    }
+}
